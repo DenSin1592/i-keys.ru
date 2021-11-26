65adca3bd74ff0ef42b40d646216aeaed596b3bb
atemiks <atemiks@gmail.com>
2021-11-26 07:35:17 +0200

    Верстка корзины

--- public/html/js/forms.js
+++ public/html/js/forms.js
@@ -1,5 +1,38 @@
 $(function () {
+    // custom select
     $('.custom-select').select2({
         minimumResultsForSearch: Infinity
+    });
+
+    // custom number
+    $(".custom-number .custom-number-button").on("click", function(e) {
+        let target = $(e.currentTarget),
+            container = target.closest('.custom-number'),
+            input = container.find("input"),
+            inputData = input.data('type'),
+            oldValue = parseFloat(input.val()),
+            newValue = undefined;
+
+        if (target.hasClass('custom-number-increase')) {
+            newValue = oldValue + 1;
+        } else {
+            if (oldValue > 0) {
+                newValue = oldValue - 1;
+            } else {
+                newValue = 0;
+            }
+        }
+
+        input.val(newValue);
+    });
+
+    // custom options
+    $('.form-option').on('click', function(e) {
+        let target = $(e.currentTarget);
+        let container = target.closest('.form-options-group');
+        let children = container.find('.form-option');
+
+        children.removeClass('active');
+        target.addClass('active');
     });
 })
