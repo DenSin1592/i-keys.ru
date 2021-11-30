59e03f6a4f2ef6951578ddc67a5571d7bc0835cf
Denis Lyubavin <lyubavin.denis@diol-it.ru>
2021-11-30 16:41:13 +0300

    Правки по баннеру whatsapp

--- resources/views/client/categories/_products_grid/_block.blade.php
+++ resources/views/client/categories/_products_grid/_block.blade.php
@@ -7,9 +7,17 @@
 
             @include('client.categories._products_grid._element')
 
-            @if($loop->iteration === 4)
+            @if($loop->iteration === 4 || trim(Setting::get("site_content.wa_phone")) !== '')
                 <div class="product-item col-sm-6 col-md-4 col-lg-3 col-xl-4 d-flex">
-                   Тут будет баннер ватсап
+                    <a href="{{Setting::get("site_content.wa_phone")}}" class="card-banner card-banner-portrait card-banner-whatsapp d-flex flex-column align-items-center justify-content-center">
+                        <div class="card-banner-thumbnail">
+                            <img loading="lazy" src="{{asset('/images/client/icons/icon-whatsapp-white.svg')}}" width="125" height="124" alt="Связаться по WhatsApp" class="card-banner-media">
+                        </div>
+
+                        <div class="card-banner-title title-h3">Свяжитесь с нами по WhatsApp</div>
+
+                        <div class="card-banner-description">Если у вас появились вопросы в выборе замка. Мы с радостью Вас проконсультируем.</div>
+                    </a>
                 </div>
                 @endif
 
