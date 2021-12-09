22cac3fde4e290cd63e71d08a94cbb05a2819ded
Denis Lyubavin <lyubavin.denis@diol-it.ru>
2021-12-09 12:36:48 +0300

    Рефакторинг

--- app/Http/Controllers/Client/OrdersController.php
+++ app/Http/Controllers/Client/OrdersController.php
@@ -10,54 +10,17 @@ use App\Services\Mailers\OrderMailer;
 use App\Models\Order;
 use Illuminate\Http\Request;
 
-//use App\Models\ClientUser;
-//use App\Models\MetaPage;
-//use App\Models\Node;
-//use App\Services\Breadcrumbs\Factory as Breadcrumbs;
-//use App\Services\DataProviders\Order\OrderCheckoutProvider;
-//use App\Services\OrderCheckout\OrderCheckout;
-//use App\Services\Repositories\Node\EloquentNodeRepository;
-//use App\Services\Repositories\Order\EloquentOrderRepository;
-//use App\Services\Seo\MetaHelper;
-//use App\Services\Validation\OrderCheckout\OrderCheckoutStep1LaravelValidator;
-//use App\Services\Validation\OrderCheckout\OrderCheckoutStep2LaravelValidator;
-
 
 class OrdersController extends Controller
 {
     use CartDataHelper;
     use DeviceDetection;
 
-//    const DEFAULT_PAGE_NAME = 'Оформление заказа';
-
-//    private $nodeRepository;
-//    private $orderRepository;
-//    private $orderCheckoutProvider;
-//    private $step1Validator;
-//    private $step2Validator;
-//    private $orderCheckout;
-//    public $orderFormProcessor;
-//    private $metaHelper;
-//    private $breadcrumbs;
-//    private $orderMailer;
-
-    /**
-     * OrdersController constructor.
-     * @param ClientOrderFormProcessor $orderFormProcessor
-     * @param OrderMailer $orderMailer
-     */
     public function __construct(
-//        private EloquentNodeRepository $nodeRepository,
-//        private EloquentOrderRepository $orderRepository,
-//        private OrderCheckoutProvider $orderCheckoutProvider,
-//        private OrderCheckoutStep1LaravelValidator $step1Validator,
-//        private OrderCheckoutStep2LaravelValidator $step2Validator,
-//        private OrderCheckout $orderCheckout,
-//        MetaHelper $metaHelper,
-//        Breadcrumbs $breadcrumbs,
         public ClientOrderFormProcessor $orderFormProcessor,
         private OrderMailer $orderMailer
     ) {}
+
 
     public function storeFast(Request $request)
     {
@@ -78,25 +41,16 @@ class OrdersController extends Controller
             );
         }
 
-        $orderCompleteContent = '<p>Вашему заказу присвоен номер ' . $order->id .
-            '. Наш менеджер свяжется с Вами в ближайшее время.</p>';
+        $orderCompleteContent = '<p>Вашему заказу присвоен номер ' . $order->id . '. Наш менеджер свяжется с Вами в ближайшее время.</p>';
 
-        try {
-            $this->orderMailer->sendAdminCompleteEmail($order);
-            $this->orderMailer->sendClientCompleteEmail($order);
-        } catch (\Throwable $e){
-            //неплохо бы логировать
-        }
+        $this->orderMailer->sendAdminCompleteEmail($order);
+        $this->orderMailer->sendClientCompleteEmail($order);
 
         return \Response::json(
             [
                 'status' => 'success',
                 'modal_title' => 'Спасибо за Ваш заказ!',
                 'modal_body' => $orderCompleteContent,
-// если нужно добавить больше информации для овтета пользователю
-//                'cart_items_content' => $this->getCartItemsContent(),
-//                'top_cart_content' => $this->getTopCartContent(),
-//                'cart_empty_content' => $this->getCartEmptyContent(),
             ]
         );
     }
--- public/js/client/quick_order.js
+++ public/js/client/quick_order.js
@@ -1,70 +1,69 @@
-(function ($, document) {
-    $(function () {
-        let modalMessage = $('#modalMessage');
-        let updateCartIcon = (count) => {
-            $('.cart-item-count').text(count);
-        };
+document.addEventListener('DOMContentLoaded', function (){
 
-        var modalQuickOrderContainer = $('#modalQuickOrder', document);
-        var quickOrderForm = modalQuickOrderContainer.find('form#quick-order');
+    let modalMessage = $('#modalMessage');
+    let updateCartIcon = (count) => {
+        $('.cart-item-count').text(count);
+    };
 
-        // Quick order form handling
-        if (quickOrderForm.length === 1) {
+    let modalQuickOrderContainer = $('#modalQuickOrder', document);
+    let quickOrderForm = modalQuickOrderContainer.find('form#quick-order');
 
-            modalQuickOrderContainer.on('click', '[data-type-submit]', function (e) {
-                e.preventDefault();
-                quickOrderForm.trigger('submit');
-            });
 
-            quickOrderForm.submit(function (e) {
-                e.preventDefault();
-                console.log('1111')
-                var self = $(this);
-                var submitBtn = modalQuickOrderContainer.find('[data-type-submit]');
-                if (!$(quickOrderForm).data('process')) {
-                    $(quickOrderForm).data('process', true);
-                    submitBtn.addClass('btn-loading');
-                    console.log('222')
+    if (quickOrderForm.length === 1) {
 
-                    $.ajax({
-                        url: self.data('action'),
-                        type: self.data('method'),
-                        data: quickOrderForm.serialize(),
-                        success: function (response) {
-                            console.log(response)
-                            if (response['status'] === 'success') {
-                                console.log(response['status'])
-                                modalQuickOrderContainer.modal('hide')
-                                modalMessage.find('h3').text(response['modal_title']);
-                                modalMessage.find('.modal-body').replaceWith(response['modal_body']);
-                                customNumberButtonInit();
-                                updateCartIcon(0);
-                                modalMessage.modal('show');
-                            } else if (response['status'] === 'error') {
-                                var errors = response['errors'];
-                                var errorsObj = {};
-                                for (var field in errors) {
-                                    errorsObj[field] = errors[field].join('<br>');
-                                }
+        modalQuickOrderContainer.on('click', '[data-type-submit]', function (e) {
+            e.preventDefault();
+            quickOrderForm.trigger('submit');
+        });
+
+        quickOrderForm.submit(function (e) {
+            e.preventDefault();
+            let self = $(this);
+            let submitBtn = modalQuickOrderContainer.find('[data-type-submit]');
+            if (!$(quickOrderForm).data('process')) {
+                $(quickOrderForm).data('process', true);
+                submitBtn.addClass('btn-loading');
+
+                $.ajax({
+                    url: self.data('action'),
+                    type: self.data('method'),
+                    data: quickOrderForm.serialize(),
+                    success: function (response) {
+                        console.log(response)
+                        if (response['status'] === 'success') {
+                            modalQuickOrderContainer.modal('hide')
+                            modalMessage.find('h3').text(response['modal_title']);
+                            modalMessage.find('.modal-body').replaceWith(response['modal_body']);
+                            customNumberButtonInit();
+                            updateCartIcon(0);
+                            modalMessage.modal('show');
+                        } else if (response['status'] === 'error') {
+                            let errors = response['errors'];
+                            let errorsObj = {};
+                            for (let field in errors) {
+                                errorsObj[field] = errors[field].join('<br>');
                             }
-                            $(quickOrderForm).data('process', false);
-                            submitBtn.removeClass('btn-loading');
-                        },
-                        error: function () {
-                            $(quickOrderForm).data('process', false);
-                            submitBtn.removeClass('btn-loading');
                         }
-                    });
-                }
+                        $(quickOrderForm).data('process', false);
+                        submitBtn.removeClass('btn-loading');
+                    },
+                    error: function () {
+                        $(quickOrderForm).data('process', false);
+                        submitBtn.removeClass('btn-loading');
+                    }
+                });
+            }
 
-                return false;
-            });
+            return false;
+        });
 
-            $(document).on('cart:removed', function (event, data) {
-                if (data['summary_count'] === 0) {
-                    modalQuickOrderContainer.modal('hide');
-                }
-            });
-        }
-    });
-})(jQuery, document);
+        $(document).on('cart:removed', function (event, data) {
+            if (data['summary_count'] === 0) {
+                modalQuickOrderContainer.modal('hide');
+            }
+        });
+    }
+
+})
+
+
