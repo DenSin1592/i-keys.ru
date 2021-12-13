50e1bf5b821e56a025ae8c41fc74e60961504671
Denis Lyubavin <lyubavin.denis@diol-it.ru>
2021-12-13 15:04:35 +0300

    Правки

--- resources/views/client/categories/_products_grid/_block.blade.php
+++ resources/views/client/categories/_products_grid/_block.blade.php
@@ -9,7 +9,7 @@
 
             @if($loop->iteration === 4 && trim(Setting::get("site_content.wa_phone")) !== '')
                 <div class="product-item col-sm-6 col-md-4 col-lg-3 col-xl-4 d-flex">
-                    <a href="{{Setting::get("site_content.wa_phone")}}" class="card-banner card-banner-portrait card-banner-whatsapp d-flex flex-column align-items-center justify-content-center">
+                    <a href="https://wa.me/{{Setting::get("site_content.wa_phone")}}" class="card-banner card-banner-portrait card-banner-whatsapp d-flex flex-column align-items-center justify-content-center">
                         <div class="card-banner-thumbnail">
                             <img loading="lazy" src="{{asset('/images/client/icons/icon-whatsapp-white.svg')}}" width="125" height="124" alt="Связаться по WhatsApp" class="card-banner-media">
                         </div>
