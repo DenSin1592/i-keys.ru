aa1f6caa9663948c82389f70d586e30d7167ec1f
Denis Lyubavin <lyubavin.denis@diol-it.ru>
2021-12-21 13:43:40 +0300

    Рефакторинг

--- app/Services/DataProviders/ClientProduct/ClientProduct.php
+++ app/Services/DataProviders/ClientProduct/ClientProduct.php
@@ -24,10 +24,16 @@ class ClientProduct
             $productData = array_merge($productData, $plugin->getForProduct($product));
         }
 
-        $productData['count_keys_in_set'] = isset($productData['attributesData']['main'][AttributeConstants::COUNT_KEYS_IN_SET_ID]['values'][0])
-            ? (int) $productData['attributesData']['main'][AttributeConstants::COUNT_KEYS_IN_SET_ID]['values'][0]
-            : null;
+        $productData['count_keys_in_set'] = $this->getCountKeys($productData);
 
         return $productData;
+    }
+
+
+    private function getCountKeys($productData): ?int
+    {
+        return isset($productData['attributesData'][AttributeConstants::MAIN][AttributeConstants::COUNT_KEYS_IN_SET_ID]['values'][0])
+            ? (int) $productData['attributesData'][AttributeConstants::MAIN][AttributeConstants::COUNT_KEYS_IN_SET_ID]['values'][0]
+            : null;
     }
 }
