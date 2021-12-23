84943eb29bf2c05cb25118b72ee728312e4a1fbf
Nikolay Evloshevskiy <evloshevsky@gmail.com>
2021-12-24 00:50:07 +0300

    Станица Услуги. промежуточный коммит 4

--- app/Http/Controllers/Client/ServicesController.php
+++ app/Http/Controllers/Client/ServicesController.php
@@ -2,25 +2,45 @@
 
 use App\Http\Controllers\Controller;
 use App\Models\Node;
+use App\Models\Service;
+use App\Services\Breadcrumbs\Container as BreadcrumbsContainer;
+use App\Services\Breadcrumbs\Factory as Breadcrumbs;
 use App\Services\Repositories\Node\EloquentNodeRepository;
 use App\Services\Seo\MetaHelper;
+use Illuminate\View\View;
 
 class ServicesController extends Controller
 {
     public function __construct(
         private EloquentNodeRepository $nodeRepository,
         private MetaHelper $metaHelper,
+        private Breadcrumbs $breadcrumbs
     ){}
 
     public function __invoke()
     {
         $node = $this->nodeRepository->findByType(Node::TYPE_SERVICES_PAGE);
         $page = \TypeContainer::getContentModelFor($node);
+        $breadcrumbs = $this->getBreadcrumbs();
+        $services = Service::where('publish', true)->orderBy('position')->get();
+        $servicesFirstBlock = $services->take(2);
+        $servicesSecondBlock = $services->splice(2);
 
-        return response('Hello World', 200);
-//            ->with('homePage', $page)
-//            ->with('authEditLink', route('cc.service-pages.edit', $page->node_id))
-//            ->with('metaData', $this->metaHelper->getRule()->metaForObject($page, $node->name));
 
+        return \View::make('client.service.index')
+            ->with('breadcrumbs', $breadcrumbs)
+            ->with('authEditLink', route('cc.service-pages.edit', $page->node_id))
+            ->with('metaData', $this->metaHelper->getRule()->metaForObject($page, $node->name))
+            ->with('servicesFirstBlock', $servicesFirstBlock)
+            ->with('servicesSecondBlock', $servicesSecondBlock);
+
+    }
+
+    private function getBreadcrumbs(): BreadcrumbsContainer
+    {
+        $breadcrumbs = $this->breadcrumbs->init();
+        $breadcrumbs->add('Главная', route('home'));
+        $breadcrumbs->add('Услуги');
+        return $breadcrumbs;
     }
 }
---
+++ resources/views/client/service/index.blade.php
@@ -0,0 +1,113 @@
+@extends('client.layouts.default')
+
+
+@section('body_class')
+    class='services-page d-flex flex-column'
+@endsection
+
+
+@section('content')
+    <main class="main-box flex-shrink-0 flex-grow-1">
+        <section class="section-display section-dark" >
+            <div class="container">
+                @include('client.shared.breadcrumbs._breadcrumbs')
+
+                <h1 class="display-title title-h1 text-uppercase">Услуги</h1>
+            </div>
+        </section>
+
+        <section class="section-posts section-dark">
+            <div class="container">
+                <div class="row">
+                    <div class="section-posts-column col-lg-6">
+                        <div class="posts-grid row">
+                            @foreach($servicesFirstBlock as $service)
+                            <div class="post-item col-12">
+                                <div class="card-post">
+                                    <div class="row">
+                                        <div class="card-post-thumbnail-container col-sm-4 col-xl-6">
+                                            <a href="text-page.html" class="card-post-thumbnail">
+                                                <img loading="lazy" src="uploads/posts/post-thumbnail-1.jpg" width="395" height="287" alt="{{$service->name}}" class="card-post-media">
+                                            </a>
+                                        </div>
+
+                                        <div class="card-post-typography-container col-sm-8 col-xl-6">
+                                            <div class="card-post-title title-h2">
+                                                <a href="#">{{$service->name}}</a>
+                                            </div>
+
+                                            <div class="card-post-description">{{$service->description}}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            @endforeach
+                        </div>
+                    </div>
+
+                    <div class="section-posts-column col-lg-6">
+                        <div class="card-post card-post-featured" style="background-image: url('uploads/posts/post-featured-thumbnail-1.jpg');" >
+                            <div class="card-post-inner d-flex flex-column align-items-start">
+                                <div class="card-post-title title-h2">
+                                    <a href="#">Мастер-системы</a>
+                                </div>
+
+                                <div class="card-post-description">Мастер-система представляет собой особую систему контроля и управления специальным доступом, принцип работы которой заключается в механическом</div>
+
+                                <a href="#" class="card-post-cta btn mt-auto" >Подробнее</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+                <a href="#link" class="card-banner card-banner-landscape card-banner-whatsapp">
+                    <div class="row">
+                        <div class="card-banner-thumbnail-container col-2 col-md-1 d-flex justify-content-center">
+                            <div class="card-banner-thumbnail">
+                                <img loading="lazy" src="images/icons/icon-whatsapp-white.svg" width="62" height="62" alt="Связаться по WhatsApp" class="card-banner-media">
+                            </div>
+                        </div>
+
+                        <div class="card-banner-typography-container col-10 col-md-11 align-self-center">
+                            <div class="card-banner-title title-h3">Свяжитесь с нами по WhatsApp</div>
+
+                            <div class="card-banner-description">Если у вас появились вопросы в выборе замка. Мы с радостью Вас проконсультируем.</div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="container">
+                <div class="posts-grid row">
+                    @foreach($servicesSecondBlock as $service)
+                    <div class="post-item col-lg-6">
+                        <div class="card-post">
+                            <div class="row">
+                                <div class="card-post-thumbnail-container col-sm-4 col-xl-6">
+                                    <a href="text-page.html" class="card-post-thumbnail">
+                                        <img loading="lazy" src="uploads/posts/post-thumbnail-3.jpg" width="395" height="287" alt="{{$service->name}}" class="card-post-media">
+                                    </a>
+                                </div>
+
+                                <div class="card-post-typography-container col-sm-8 col-xl-6">
+                                    <div class="card-post-title title-h2">
+                                        <a href="#">{{$service->name}}</a>
+                                    </div>
+
+                                    <div class="card-post-description">{{$service->description}}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+        </section>
+    </main>
+@endsection
+
+@section('modals')
+
+@endsection
