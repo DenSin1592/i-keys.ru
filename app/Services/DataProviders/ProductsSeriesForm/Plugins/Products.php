2af1beccd4278f50ff3252426042b76eb43baf65
Denis Lyubavin <lyubavin.denis@diol-it.ru>
2021-12-27 18:29:40 +0300

    Сделал что бы в серии не выводились товары из других категорий

--- app/Services/DataProviders/ProductsSeriesForm/Plugins/Products.php
+++ app/Services/DataProviders/ProductsSeriesForm/Plugins/Products.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\DataProviders\ProductsSeriesForm\Plugins;
 
+use App\Models\Attribute\AttributeConstants;
 use App\Services\DataProviders\ProductsSeriesForm\ProductsSeriesFormPlugin;
 
 
@@ -9,6 +10,25 @@ class Products implements ProductsSeriesFormPlugin
 {
     public function getSubData($item, array $additionalData = []): array
     {
-        return ['products' => $item->productsForSingle];
+        $products = $item->productsForSingle;
+
+
+        if ($item->attribute_id === AttributeConstants::LOCK_SERIES_ID){
+            foreach ($products as $key => $element){
+                if(!$element->isLock()){
+                    $products->forget($key);
+                }
+            }
+        }
+
+        if ($item->attribute_id === AttributeConstants::CYLINDER_SERIES_ID){
+            foreach ($products as $key => $element){
+                if(!$element->isCylinder()){
+                    $products->forget($key);
+                }
+            }
+        }
+
+        return ['products' => $products];
     }
 }
