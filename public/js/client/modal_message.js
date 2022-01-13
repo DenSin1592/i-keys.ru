59f6818f0988b2daef059c4b2073d44cbb65e3cd
Denis Lyubavin <lyubavin.denis@diol-it.ru>
2022-01-13 13:47:13 +0300

    Правки по модальным окнам

--- public/js/client/modal_message.js
+++ public/js/client/modal_message.js
@@ -4,25 +4,39 @@ document.addEventListener('DOMContentLoaded', () => {
     document.modalMessage = $('#modalMessage');
     const modalMessageStartContent = document.modalMessage.html();
 
+
     document.modalMessage.on('hidden.bs.modal', () => {
         document.modalMessage.html(modalMessageStartContent);
     });
 
 
     document.modalMessageShow = (header, body) => {
-        document.modalMessage.find('h3').text(header);
-        document.modalMessage.find('.modal-body').replaceWith(body);
-        document.modalMessage.modal('show');
-    }
+        closeAllModals();
+        setTimeout(() => {
+            document.modalMessage.find('h3').text(header);
+            document.modalMessage.find('.modal-body').replaceWith(body);
+            document.modalMessage.modal('show');
+        }, 1000);
+    };
 
 
     document.modalMessageErrorShow = () => {
-        $('.modal').hide();
-        document.modalMessage.html(modalMessageStartContent);
-        document.modalMessage.find('h3').text('Что то пошло не так...');
-        document.modalMessage.find('.modal-body').replaceWith('<div class="modal-body"><div class="form-modal form"><div  class="title-h4 text-secondary">Наши специалисты уже работают над устранение проблемы.</div></div>');
-        document.modalMessage.find('.modal-footer').replaceWith('<div class="modal-footer"> Пожалуйста, попробуйте позже.</div>');
-        document.modalMessage.modal('show');
-    }
+        closeAllModals();
+        setTimeout(() => {
+            document.modalMessage.find('h3').text('Что то пошло не так...');
+            document.modalMessage.find('.modal-body').replaceWith('<div class="modal-body"><div class="form-modal form"><div  class="title-h4 text-secondary">Наши специалисты уже работают над устранение проблемы.</div></div>');
+            document.modalMessage.find('.modal-footer').replaceWith('<div class="modal-footer"> Пожалуйста, попробуйте позже.</div>');
+            document.modalMessage.modal('show');
+        }, 1000);
+    };
+
+
+    let closeAllModals = () => {
+        setTimeout(() => {
+            $('.modal').each((_, elem) => {
+                $(elem).modal('hide');
+            });
+        }, 200);
+    };
 
 });
