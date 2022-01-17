b3607164ff643103ac972d1b65743315bee8f423
Denis Lyubavin <lyubavin.denis@diol-it.ru>
2022-01-17 17:35:51 +0300

    Правки

--- public/js/client/change_product_card.js
+++ public/js/client/change_product_card.js
@@ -5,35 +5,43 @@ document.addEventListener('DOMContentLoaded', function () {
     const URL_CHANGE_CARD = '/change-product-card'
 
     $(document).on('change', 'input.change-card, select.change-card', (e) => {
-        e.preventDefault();
-        let input = $(e.currentTarget);
-        let productCard = input.closest('.product-item');
-        let productCardContent = input.closest('.card-product');
 
-        productCard.find('input.change-card').attr("disabled", "disabled");
-        productCard.find('a').attr('href', 'javascript:;')
-        productCard.stop().animate({opacity: 0}, TIME_OPACITY)
+        try {
+            e.preventDefault();
+            let input = $(e.currentTarget);
+            let productCard = input.closest('.product-item');
+            let productCardContent = input.closest('.card-product');
 
-        let data = {
-            productId: input.val(),
-            cardNumber: productCardContent.data('cardNumber'),
+            productCard.find('input.change-card').attr("disabled", "disabled");
+            productCard.find('a').attr('href', 'javascript:;')
+            productCard.stop().animate({opacity: 0}, TIME_OPACITY)
+
+            let data = {
+                productId: input.val(),
+                cardNumber: productCardContent.data('cardNumber'),
+            }
+
+            $.ajax({
+                method: 'POST',
+                url: URL_CHANGE_CARD,
+                data: data,
+                cache: false
+            }).done((response) => {
+                setTimeout(() => {
+                    productCardContent.replaceWith(response['content']);
+                    productCard.animate({opacity: 1}, TIME_OPACITY)
+                    initSelect2();
+                }, TIME_OPACITY_TIMEOUT)
+
+            }).fail(() => {
+                document.modalMessageErrorShow();
+                productCard.remove();
+            });
+        } catch (error) {
+            document.modalMessageErrorShow();
+            console.warn('change_product_card.js: ', error)
         }
 
-        $.ajax({
-            method: 'POST',
-            url: URL_CHANGE_CARD,
-            data: data,
-            cache: false
-        }).done((response) => {
-            setTimeout(() => {
-                productCardContent.replaceWith(response['content']);
-                productCard.animate({opacity: 1}, TIME_OPACITY)
-                initSelect2();
-            }, TIME_OPACITY_TIMEOUT)
-
-        }).fail(() => {
-            productCard.remove();
-        });
     });
 });
 
