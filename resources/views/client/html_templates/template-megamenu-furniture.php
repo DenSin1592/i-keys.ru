bcdf2138f6441f1cc29be0d6a640a65b407ac38b
atemiks <atemiks@gmail.com>
2022-01-27 20:27:35 +0200

    Верстка структуры для выпадающего меню

---
+++ resources/views/client/html_templates/template-megamenu-furniture.php
@@ -0,0 +1,18 @@
+<div class="megamenu-wrapper">
+    <div class="megamenu-inner">
+        <div class="row">
+            <div class="megamenu-column col-6 col-md-3 col-xl">
+                <div class="megamenu-block">
+                    <ul class="megamenu-list list-unstyled">
+                        <li><a href="#link">Ручки</a></li>
+                        <li><a href="#link">Петли</a></li>
+                        <li><a href="#link">Накладки</a></li>
+                        <li><a href="#link">Броненакладки</a></li>
+                        <li><a href="#link">Глазки</a></li>
+                        <li><a href="#link">Доводчик</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/client/html_templates/template-megamenu-locks.php
@@ -0,0 +1,44 @@
+<div class="megamenu-wrapper">
+    <div class="megamenu-inner">
+        <div class="row">
+            <div class="megamenu-column col-6 col-md-3 col-xl">
+                <div class="megamenu-block">
+                    <div class="megamenu-title font-family-secondary">Бренд</div>
+                    <ul class="megamenu-list list-unstyled">
+                        <li><a href="#link">ALMAR</a></li>
+                        <li><a href="#link">CISA</a></li>
+                        <li><a href="#link">MOTTURA (Моттура)</a></li>
+                        <li><a href="#link">SECUREMME</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="megamenu-column col-6 col-md-3 col-xl">
+                <div class="megamenu-block">
+                    <div class="megamenu-title font-family-secondary">Тип замка</div>
+                    <ul class="megamenu-list list-unstyled">
+                        <li><a href="#link">Врезной</a></li>
+                        <li><a href="#link">Навесной</a></li>
+                        <li><a href="#link">Накладной</a></li>
+                        <li><a href="#link">Накладной, электромеханический</a></li>
+                        <li><a href="#link">Электромеханический, врезной</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="megamenu-column col-6 col-md-3 col-xl">
+                <div class="megamenu-block">
+                    <div class="megamenu-title font-family-secondary">Механизм секретности</div>
+                    <ul class="megamenu-list list-unstyled">
+                        <li><a href="#link">Комбинированный сувальдно-цилиндровый</a></li>
+                        <li><a href="#link">Помповый</a></li>
+                        <li><a href="#link">Сувальдный</a></li>
+                        <li><a href="#link">Сувальдный с перекодировкой</a></li>
+                        <li><a href="#link">Сувальдный с постоянным кодом</a></li>
+                        <li><a href="#link">Цилиндровый</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
