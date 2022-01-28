eb9cc2961d738f17f46018260ddae5d759e290fb
Nikolay Evloshevskiy <evloshevsky@gmail.com>
2022-01-28 13:16:39 +0300

    добавлено - миниатюра товара в модальном окне покупки

--- resources/views/client/shared/modal/_success_in_cart.blade.php
+++ resources/views/client/shared/modal/_success_in_cart.blade.php
@@ -2,19 +2,17 @@
     <form action="#" class="form-modal form">
         <div class="form-row">
             <div class="col-3 col-sm-2">
-                {{-- todo: заменить ссылку --}}
-                <a href="#link" class="modal-thumbnail d-flex align-items-center justify-content-center">
+                <a href="{{\UrlBuilder::getUrl($product)}}" class="modal-thumbnail d-flex align-items-center justify-content-center">
                     <img loading="lazy" src="{{$product->getFirstImagePath('image', 'catalog', 'no-image-200x200.png')}}" alt="{{$product->name}}" class="modal-media">
                 </a>
             </div>
 
             <div class="col-9 col-sm-10 align-self-center">
-                {{-- todo: заменить ссылку --}}
-                <a href="#link" id="modalAddToCardProductName" class="title-h4 text-secondary">{!! $product->name !!}</a>
-        
+                <a href="{{\UrlBuilder::getUrl($product)}}" id="modalAddToCardProductName" class="title-h4 text-secondary">{!! $product->name !!}</a>
+
                 <div class="form-group d-flex align-items-center">
                     <label for="modalAddToCartQuantity" class="form-label form-label-column">Кол-во:</label>
-        
+
                     <div class="custom-number custom-control d-flex align-items-center">
                         <button type="button"
                                 class="custom-number-button custom-number-decrease d-flex align-items-center justify-content-center">
@@ -22,9 +20,9 @@
                                 <use xlink:href="{{asset('/images/client/sprite.svg#icon-minus')}}"></use>
                             </svg>
                         </button>
-        
+
                         <input type="number" id="modalAddToCartQuantity" class="custom-number-input update-product-count-in-cart" value="{{$count}}">
-        
+
                         <button type="button"
                                 class="custom-number-button custom-number-increase d-flex align-items-center justify-content-center">
                             <svg class="custom-number-button-media" width="12" height="12">
