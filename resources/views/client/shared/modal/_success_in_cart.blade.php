9b0b14a03398315f787a3ea787a7637d534d4b29
atemiks <atemiks@gmail.com>
2022-01-28 11:52:39 +0200

    Верстка картинки в модальном окне "добавить товар в корзину"

--- resources/views/client/shared/modal/_success_in_cart.blade.php
+++ resources/views/client/shared/modal/_success_in_cart.blade.php
@@ -1,27 +1,36 @@
 <div class="modal-body" data-cart-product-id="{{$product->id}}">
     <form action="#" class="form-modal form">
-{{--        <img loading="lazy" src="{{$product->getFirstImagePath('image', 'catalog', 'no-image-200x200.png')}}" alt="{{$product->name}}" class="card-product-media">--}}
-        <div id="modalAddToCardProductName" class="title-h4 text-secondary">{!! $product->name !!}</div>
+        <div class="form-row">
+            <div class="col-3 col-sm-2">
+                <div class="modal-thumbnail d-flex align-items-center justify-content-center">
+                    <img loading="lazy" src="{{$product->getFirstImagePath('image', 'catalog', 'no-image-200x200.png')}}" alt="{{$product->name}}" class="modal-media">
+                </div>
+            </div>
 
-        <div class="form-group d-flex align-items-center">
-            <label for="modalAddToCartQuantity" class="form-label form-label-column">Кол-во:</label>
-
-            <div class="custom-number custom-control d-flex align-items-center">
-                <button type="button"
-                        class="custom-number-button custom-number-decrease d-flex align-items-center justify-content-center">
-                    <svg class="custom-number-button-media" width="12" height="12">
-                        <use xlink:href="{{asset('/images/client/sprite.svg#icon-minus')}}"></use>
-                    </svg>
-                </button>
-
-                <input type="number" id="modalAddToCartQuantity" class="custom-number-input update-product-count-in-cart" value="{{$count}}">
-
-                <button type="button"
-                        class="custom-number-button custom-number-increase d-flex align-items-center justify-content-center">
-                    <svg class="custom-number-button-media" width="12" height="12">
-                        <use xlink:href="{{asset('/images/client/sprite.svg#icon-plus')}}"></use>
-                    </svg>
-                </button>
+            <div class="col-9 col-sm-10 align-self-center">
+                <div id="modalAddToCardProductName" class="title-h4 text-secondary">{!! $product->name !!}</div>
+        
+                <div class="form-group d-flex align-items-center">
+                    <label for="modalAddToCartQuantity" class="form-label form-label-column">Кол-во:</label>
+        
+                    <div class="custom-number custom-control d-flex align-items-center">
+                        <button type="button"
+                                class="custom-number-button custom-number-decrease d-flex align-items-center justify-content-center">
+                            <svg class="custom-number-button-media" width="12" height="12">
+                                <use xlink:href="{{asset('/images/client/sprite.svg#icon-minus')}}"></use>
+                            </svg>
+                        </button>
+        
+                        <input type="number" id="modalAddToCartQuantity" class="custom-number-input update-product-count-in-cart" value="{{$count}}">
+        
+                        <button type="button"
+                                class="custom-number-button custom-number-increase d-flex align-items-center justify-content-center">
+                            <svg class="custom-number-button-media" width="12" height="12">
+                                <use xlink:href="{{asset('/images/client/sprite.svg#icon-plus')}}"></use>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     </form>
