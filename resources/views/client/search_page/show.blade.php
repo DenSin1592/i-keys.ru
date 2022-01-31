d3d63418b4dd4870070523f9f4dee3a95f8305a3
Denis Lyubavin <lyubavin.denis@diol-it.ru>
2022-01-31 15:37:46 +0300

    Правки по поиску

--- resources/views/client/search_page/show.blade.php
+++ resources/views/client/search_page/show.blade.php
@@ -20,9 +20,9 @@
                     </svg>
 
                     @if ($paginator->total() > 0)
-                        Найдено {{ trans_choice('messages.product', $paginator->total()) }} по запросу "{{ $query }}" {{Request::get('category_for_search') !== 'all' ? 'в разделе "'. \App\Models\Category::MAPPING_ALIASES[Request::get('category_for_search')].'"' : '' }}.
+                        Найдено {{ trans_choice('messages.product', $paginator->total()) }} по запросу "{{ $query }}"{{Request::get('category_for_search') !== 'all' ? ' в разделе "'. \App\Models\Category::MAPPING_ALIASES[Request::get('category_for_search')].'"' : '' }}.
                     @else
-                        <p>По запросу "{{ $query }}" не найдено ни одного товара {{Request::get('category_for_search') !== 'all' ? 'в разделе "'. \App\Models\Category::MAPPING_ALIASES[Request::get('category_for_search')].'"' : '' }}.</p>
+                        <p>По запросу "{{ $query }}" не найдено ни одного товара{{Request::get('category_for_search') !== 'all' ? ' в разделе "'. \App\Models\Category::MAPPING_ALIASES[Request::get('category_for_search')].'"' : '' }}.</p>
                     @endif
                 </div>
             </div>
